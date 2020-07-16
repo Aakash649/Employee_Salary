0e188dd6a9f2524e242c6d16f0d816ce8e706a1b
Aakash Malvankar <10ajax649hamilton@gmail.com>
2020-07-16 23:41:15 +0530

    Attendence

--- Emp_Sal.sh
+++ Emp_Sal.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+
+x=$((RANDOM % 2))
+
+if [ $x -eq 1 ]
+then
+	echo "Is Present"
+else
+	echo "Is Absent"
+fi
