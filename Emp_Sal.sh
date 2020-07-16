65a6b86828bcd51eb05b1f8f610d57a12459ebc6
Aakash Malvankar <10ajax649hamilton@gmail.com>
2020-07-17 00:04:39 +0530

    Part_Time Wage

--- Emp_Sal.sh
+++ Emp_Sal.sh
@@ -4,15 +4,21 @@ echo "Welcome to Employee Wage Computation Program on Master Branch"
 
 WagePerHr=20
 FullDay=8
+PartDay=4
+Dayoff=0
 
 x=$((RANDOM % 2))
 
-if [ $x -eq 1 ]
-then
-	echo "Is Present"
-	y=$(($WagePerHr * $FullDay))
-	echo "Wage is : " $y
-else
-	echo "Is Absent"
-fi
+	case $x in
+	1) 
+	y=$(($Fullday * $WagePerHr))
+	;;
+	2)
+	y=$(($Partday * $WagePerHr))
+	;;
+	3)
+	y=$(($DayOff * $WagePerHr))
+	;;
+	esac
 
+echo "Wage is :- " $y
