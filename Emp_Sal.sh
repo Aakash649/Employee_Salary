55e269ebe76d126341133b292c7a7bfa43e6032b
Aakash Malvankar <10ajax649hamilton@gmail.com>
2020-07-16 23:48:12 +0530

    Employee Wage

--- Emp_Sal.sh
+++ Emp_Sal.sh
@@ -2,11 +2,17 @@
 
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 
+WagePerHr=20
+FullDay=8
+
 x=$((RANDOM % 2))
 
 if [ $x -eq 1 ]
 then
 	echo "Is Present"
+	y=`expr $WagePerHr * $FullDay`
+	echo "Wage is : " $y
 else
 	echo "Is Absent"
 fi
+
